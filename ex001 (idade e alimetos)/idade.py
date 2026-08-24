0a45a7768f092b1ac5c75f6edc7fe792e89ca2bd
Guilherme da Silva <gl.guilherme2007@gmail.com>
2026-08-24 15:06:46 -0300

    exercicios de media das idade

---
+++ ex001 (idade e alimetos)/idade.py
@@ -0,0 +1,17 @@
+#idade de 4 pessoas
+#dividir por 4 (media)
+
+idade1 = int(input("Digite a idade da primeira pessoa: "))
+
+idade2 =  int(input("Digite a idade da segunda pessoa: ")) 
+
+idade3 =  int(input("Digite a idade da terceira pessoa: "))
+                    
+idade4 =  int(input("Digite a idade da quarta pessoa: "))
+
+
+#media
+media = (idade1 + idade2 + idade3 + idade4) / 4
+
+#exibir
+print(f"A media e:{media}")
